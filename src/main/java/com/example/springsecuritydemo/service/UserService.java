68290555b08b911f2b59b08310b4215d829c2c32
Mark <lakesd6531@gmail.com>
2022-01-09 22:16:53 +0800

    implement user service.

---
+++ src/main/java/com/example/springsecuritydemo/service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.springsecuritydemo.service;
+
+import com.example.springsecuritydemo.domain.Role;
+import com.example.springsecuritydemo.domain.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+    Role saveRole(Role role);
+    void addRoleToUser(String username, String roleName);
+    User getUser(String username);
+    List<User> getUsers();
+}
