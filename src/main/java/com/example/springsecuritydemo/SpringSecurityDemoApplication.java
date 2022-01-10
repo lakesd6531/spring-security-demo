0821bbd6a017ab329bd56428a1e85f7b90eb37e4
Mark <lakesd6531@gmail.com>
2022-01-10 15:34:06 +0800

    add data when init.

--- src/main/java/com/example/springsecuritydemo/SpringSecurityDemoApplication.java
+++ src/main/java/com/example/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -1,13 +1,43 @@
 package com.example.springsecuritydemo;
 
+import com.example.springsecuritydemo.domain.Role;
+import com.example.springsecuritydemo.domain.User;
+import com.example.springsecuritydemo.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SpringSecurityDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityDemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            userService.saveRole(new Role(null, "ROLE_USER"));
+            userService.saveRole(new Role(null, "ROLE_MANAGER"));
+            userService.saveRole(new Role(null, "ROLE_ADMIN"));
+            userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+            userService.saveUser(new User(null, "John", "john", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Will", "will", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Jim", "jim", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Mark", "mark", "1234", new ArrayList<>()));
+
+            userService.addRoleToUser("john", "ROLE_USER");
+            userService.addRoleToUser("john", "ROLE_MANAGER");
+            userService.addRoleToUser("will", "ROLE_MANAGER");
+            userService.addRoleToUser("jim", "ROLE_ADMIN");
+            userService.addRoleToUser("mark", "ROLE_SUPER_ADMIN");
+            userService.addRoleToUser("mark", "ROLE_ADMIN");
+            userService.addRoleToUser("mark", "ROLE_USER");
+        };
     }
 
 }
