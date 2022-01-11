38ebac27b88f846096cfeda87df9be801c28f5fe
Mark <mark.zhang@tpisoftware.com>
2022-01-11 15:12:24 +0800

    implement authorization filter.

--- src/main/java/com/example/springsecuritydemo/security/SecurityConfig.java
+++ src/main/java/com/example/springsecuritydemo/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.springsecuritydemo.security;
 
 import com.example.springsecuritydemo.filter.CustomAuthenticationFilter;
+import com.example.springsecuritydemo.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
@@ -40,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
